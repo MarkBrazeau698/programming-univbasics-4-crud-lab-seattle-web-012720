24fd98f93b70704c77a0d3b86b0501f83dfa89ee
Mark Brazeau <kitsapbackflow@gmail.com>
2019-12-19 22:01:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,7 +25,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = ["I", "am"]
+  array[1]
 end
 
 def retrieve_first_element_from_array(array)
